3889c05fe88cd5db087212984617e41006250b74
Merve <merve.maths@gmail.com>
2021-04-02 22:33:14 +0200

    initial commit

---
+++ src/main/java/Homework/TestNgHomework/FirstTest.java
@@ -0,0 +1,11 @@
+package Homework.TestNgHomework;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void firstTest(){
+		System.out.println("Hello World");
+	}
+}
